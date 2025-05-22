565eaac737a1b90290662672c438be586335404e
Vishang <vishang.personal@gmail.com>
2025-05-22 20:06:18 +0530

    fixed prisma db issue

--- db/prisma.ts
+++ db/prisma.ts
@@ -7,14 +7,15 @@ import ws from 'ws';
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 neonConfig.webSocketConstructor = ws;
 const connectionString = `${process.env.DATABASE_URL}`;
-//push
+// console.log('process.env.DATABASE_URL',process.env.DATABASE_URL)
+
 // Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
+// const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
 
 
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
 //@ts-ignore
-const adapter = new PrismaNeon(pool);
+const adapter = new PrismaNeon({ connectionString });
 
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
 export const prisma = new PrismaClient({ adapter }).$extends({
