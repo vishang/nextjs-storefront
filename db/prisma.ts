e3da4d4ae7ae68f4f7edeb451e41a2ce193cac87
Vishang <vishang.personal@gmail.com>
2025-05-22 19:35:54 +0530

    fake push

--- db/prisma.ts
+++ db/prisma.ts
@@ -7,7 +7,7 @@ import ws from 'ws';
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 neonConfig.webSocketConstructor = ws;
 const connectionString = `${process.env.DATABASE_URL}`;
-
+//push
 // Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
 const pool = new Pool({ connectionString });
 
